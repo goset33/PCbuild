508e8d188ea6e28d95d29fdd4fe6e1a4afa1e18c
goset33 <gogaprofi09@gmail.com>
2024-06-29 20:22:50 +0700

    commit

--- Assets/Scripts/MailController.cs
+++ Assets/Scripts/MailController.cs
@@ -10,23 +10,64 @@ public class MailController : MonoBehaviour
     public TextMeshProUGUI mailText;
     public UnityEvent taskAccepted;
 
+    public string[] videocardsNames, processorNames, hardDiskNames, coolerNames, operativeNames, corpseNames;
+
+    [Space]
     [TextArea(3, 10)]
     public string[] textVariations;
 
     public void CreateTask()
     {
         string choosedVariation = textVariations[Random.Range(0, textVariations.Length)];
+        if (choosedVariation.Contains("[название компании]"))
+        {
+            choosedVariation.Replace("[название компании]", PlayerPrefs.GetString("Company"));
+        }
+        if (choosedVariation.Contains("*"))
+        {
+            if (choosedVariation.Contains("="))
+            {
+                choosedVariation.Replace("=", videocardsNames[Random.Range(0, videocardsNames.Length)]);
+            }
+            if (choosedVariation.Contains("+"))
+            {
+                choosedVariation.Replace("+", processorNames[Random.Range(0, processorNames.Length)]);
+            }
+            if (choosedVariation.Contains("-"))
+            {
+                choosedVariation.Replace("-", hardDiskNames[Random.Range(0, hardDiskNames.Length)]);
+            }
+            if (choosedVariation.Contains("$"))
+            {
+                choosedVariation.Replace("$", coolerNames[Random.Range(0, coolerNames.Length)]);
+            }
+            if (choosedVariation.Contains("/"))
+            {
+                choosedVariation.Replace("/", operativeNames[Random.Range(0, operativeNames.Length)]);
+            }
+            if (choosedVariation.Contains("|"))
+            {
+                choosedVariation.Replace("|", corpseNames[Random.Range(0, corpseNames.Length)]);
+            }
+        }
+        
         mailText.text = choosedVariation;
     }
 
     public void Accept()
     {
-
+        taskAccepted?.Invoke();
     }
 
     public void Cancel()
     {
+        StartCoroutine(CancelCorutine());
+    }
+
+    IEnumerator CancelCorutine()
+    {
         mailText.DOFade(0f, 1f);
+        yield return new WaitForSeconds(1f);
         CreateTask();
         mailText.DOFade(1f, 1f);
     }
