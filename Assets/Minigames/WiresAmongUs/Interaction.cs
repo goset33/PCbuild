ff9657717439fc70b8ccdb19c503f060ef8d1895
ZhekaRifer <scorpionrulit2@bk.ru>
2024-06-29 12:39:46 +0300

    Revert "Update"
    This reverts commit 7394c0fe6bdf8f503247f26b1ca0bc572c4006b0.

---
+++ Assets/Minigames/WiresAmongUs/Interaction.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using static UnityEditor.PlayerSettings;
+
+public class Interaction : MonoBehaviour
+{
+    private bool _IsDraged = false;
+
+    [HideInInspector]
+    public bool _isConnected = false;
+
+    public int indexInteraction;
+    public UnityEvent onConnected;
+
+
+    private Vector3 _startPos;
+    private LineRenderer _lineRenderer;
+
+    void Start()
+    {
+        
+        _lineRenderer = GetComponent<LineRenderer>();
+        _startPos = _lineRenderer.GetPosition(1);
+    }
+
+    public void SetDrag(bool isDrag)
+    {
+        _IsDraged = isDrag;
+    }
+    public Vector3 GetStartPos()
+    {
+        return _startPos;
+    }
+
+    public void SetNewPosLinePoint(Vector3 newPos)
+    {
+        if (_lineRenderer != null)
+        {
+            _lineRenderer.SetPosition(1, transform.InverseTransformPoint(newPos));
+        }
+    }
+
+    public LineRenderer GetLine()
+    {
+        return _lineRenderer;
+    }
+
+    public void SetIsConnected(bool isConnected, Vector3 PosConnected)
+    {
+        if (_lineRenderer != null && isConnected)
+        {
+            _lineRenderer.SetPosition(1, transform.InverseTransformPoint(PosConnected));
+            _isConnected = isConnected;
+            onConnected.Invoke();
+        }
+    }
+}
---
+++ Assets/Scripts/DayController.cs
@@ -0,0 +1,63 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DayController : MonoBehaviour
+{
+    public Image Dark;
+    public TextMeshProUGUI subtitle;
+    [Space]
+    public AudioSource source;
+    public AudioClip[] guideAudioClips;
+
+    private int day;
+
+    private void Start()
+    {
+        StartCoroutine(StartDay());
+    }
+
+    IEnumerator StartDay()
+    {
+        yield return new WaitForSeconds(0.3f);
+        if (PlayerPrefs.HasKey("Day"))
+        {
+            day = PlayerPrefs.GetInt("Day");
+        }
+        else
+        {
+            Dark.transform.parent.transform.GetChild(1).gameObject.SetActive(false);
+        }
+        Dark.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = "День " + day;
+        Dark.transform.GetChild(0).GetComponent<TextMeshProUGUI>().DOColor(new Color(255f, 255f, 255f, 255f), 500f);
+
+        yield return new WaitForSeconds(1.5f);
+        Dark.DOColor(new Color(0f, 0f, 0f, 0f), 2f);
+        Dark.transform.GetChild(0).GetComponent<TextMeshProUGUI>().DOColor(new Color(255f, 255f, 255f, 0f), 2f);
+        yield return new WaitForSeconds(2f);
+        Dark.gameObject.SetActive(false);
+        
+
+        if (day == 0)
+        {
+            subtitle.text = "Спустя столько времени я смог! Я открыл своё дело!";
+            source.PlayOneShot(guideAudioClips[0], source.volume);
+            yield return new WaitForSeconds(4.7f);
+            subtitle.text = "Теперь я могу заниматься своим любимым делом!";
+            source.PlayOneShot(guideAudioClips[1], source.volume);
+            yield return new WaitForSeconds(3f);
+            subtitle.text = "Собирать компютеры на заказ!";
+            source.PlayOneShot(guideAudioClips[2], source.volume);
+            yield return new WaitForSeconds(2.5f);
+            subtitle.text = "Пора приступать к работе";
+            source.PlayOneShot(guideAudioClips[3], source.volume);
+            yield return new WaitForSeconds(2.5f);
+            subtitle.text = "Так, нужно по почте посмотреть";
+            source.PlayOneShot(guideAudioClips[4], source.volume);
+            
+        }
+    }
+}
